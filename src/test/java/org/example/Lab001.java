2d2b95970e885a8c3204d9402842bfb543091f3a
klalasa <klalasa@gmail.com>
2024-04-23 16:36:44 +0530

    Second commit

--- src/test/java/org/example/Lab001.java
+++ src/test/java/org/example/Lab001.java
@@ -1,86 +1,97 @@
 package org.example;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import org.openqa.selenium.By;
-import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.*;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.time.Duration;
 
+
 public class Lab001 {
-        EdgeDriver driver;
 
-        @BeforeClass
+    // 2 Test cases
 
-        public void setUp(){
+    // One Negative - Invalid Username, password -> Error
+    // One Positive - Valid Username, password -> Dashboard Page
 
-            EdgeOptions options=new EdgeOptions();
-            options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-            options.addArguments("--guest");
-            driver=new EdgeDriver(options);
+    ChromeOptions options;
+    WebDriver driver;
 
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() {
+        System.out.println("Initializing WebDriver...");
+        options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+
+
+            driver = new ChromeDriver(options);
             driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.manage().deleteAllCookies();
 
-        }
-        @Test(dataProvider = "logindata")
-        public void testDataDriven(String id,String Email,String password, String ExpectedResult){
-            driver.get("https://app.vwo.com");
-            WebElement emailElement= driver.findElement(By.id("login-username"));
-            emailElement.clear();
-            emailElement.sendKeys(Email);
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-            WebElement passwordElement=driver.findElement(By.id("login-password"));
-            passwordElement.clear();
-            passwordElement.sendKeys(password);
-            driver.findElement(By.id("js-login-btn")).click();
-
-            if (ExpectedResult.equalsIgnoreCase("Invalid")){
-                WebElement err_msg=driver.findElement(By.className("notification-box-description"));
-                WebDriverWait wait= new WebDriverWait(driver,Duration.ofSeconds(10));
-                wait.until(ExpectedConditions.visibilityOf(err_msg));
-                Assert.assertTrue(err_msg.isDisplayed());
-                String err_msg_text=err_msg.getText();
-                Assert.assertEquals(err_msg_text,"Your email, password, IP address or location did not match");
-
-
-            }
-
-
-            if(ExpectedResult.equalsIgnoreCase("valid")){
-                String text=driver.findElement(By.cssSelector("[data-qa=\"lufexuloga\"]")).getText();
-                System.out.println(text);
-                Assert.assertEquals(text,"abcd test");
-            }
-
-
-        }
-        @DataProvider(name="logindata")
-        public Object[][] testData(){
-            return new Object[][]{
-
-                    {"TD1","practice@special.com","test@123","Invalid"},
-                    {"TD2","abcd@test.com","Test@abc","valid"}
-            };
-        }
-
-
-        @AfterClass
-        public void tearDown(){
-
-            driver.quit();
-        }
-
+            // Add any additional error handling or logging as needed
 
     }
 
 
+    @Test(priority = 1, groups = {"negative", "sanity", "reg"})
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("TC#1 - Verify that with Invalid username and Valid password, Login is not successfull !!")
+    public void testInvalidLogin() throws InterruptedException {
+
+
+
+
+        driver.get("https://app.vwo.com");
+        driver.findElement(By.id("login-username")).sendKeys("93npu2yyb0@esiix.co");
+        driver.findElement(By.id("login-password")).sendKeys("Wingify@123");
+        driver.findElement(By.id("js-login-btn")).click();
+
+        WebElement errorMessage = driver.findElement(By.className("notification-box-description"));
+        // 3 seconds
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(errorMessage));
+
+        String errorString = driver.findElement(By.className("notification-box-description")).getText();
+        Assert.assertEquals(errorMessage.getText(), "Your email, password, IP address or location did not match");
+
+
+    }
+
+    @Test(enabled = true, priority = 2, groups = {"positive", "sanity", "stage"})
+    @Description("Verify that with Valid username and Valid password, Login is successfull !!")
+    public void testValidLogin() throws InterruptedException {
+
+        // ID, Name, ClassName -> CssSelector, Xppath - Firefox or Chrome
+        // xpath. CSS Selector -> Chrome, will not work Firefox.
+
+
+        driver.get("https://app.vwo.com");
+        driver.findElement(By.id("login-username")).sendKeys("abcd@test.com");
+        driver.findElement(By.id("login-password")).sendKeys("Test@abc");
+        driver.findElement(By.id("js-login-btn")).click();
+
+        WebElement element = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-qa=\"lufexuloga\"]")));
+        String text= element.getText();
+        System.out.println(text);
+        Assert.assertEquals(text,"abcd test");
+
+
+
+
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+    }
+
+
+}
